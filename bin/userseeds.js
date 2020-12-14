63f4eb45e7eb1fdd33a7637548f57109300afc30
Marine Sanjuan <marine@sanjuan.eu>
2020-12-14 11:47:59 +0100

    tried to fix connection to db

--- bin/userseeds.js
+++ bin/userseeds.js
@@ -1,5 +1,5 @@
 // create a test data set of valid users
-require("dotenv");
+require("dotenv").config();
 require("./../config/mongo"); // fetch the db connection
 
 const UserModel = require("./../models/Users"); // fetch the model to validate our user document before insertion (in database)
