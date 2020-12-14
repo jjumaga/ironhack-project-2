d5dff596a94e687ead50a2af37dd5c15f9133bfc
Jacqueline <j.jumaga@gmail.com>
2020-12-14 10:55:51 +0100

    initial layout done

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+require("./config/mongo");
+
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+const SpotifyWebApi = require("spotify-web-api-node");
+const mongoose = require("mongoose");
+const path = require("path");
+
+//Define Routers
+//const memoriesRouter = require("./routes/memories");
+//const spotifyRouter = require("./routes/spotify");
+//const usersRouter = require("./routes/users");
+
+// HBS/VIEWS SET UP
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
+// SPOTIFY SET UP
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
+
+// SET PREFIXES FOR ROUTES BELOW:
+//to-do selon CRUD
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+  console.log("let's roooock @ http://localhost:" + process.env.PORT);
+});
+
+// POSSIBLE EXPORTS
+module.exports = app;
